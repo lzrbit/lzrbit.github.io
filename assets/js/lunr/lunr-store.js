11376e0e4563e0fed109ddeba161cfaf7c12ec76
lzrbit <805359852@qq.com>
2021-06-22 06:33:15 +0800

    Site updated at 2021-06-21 22:33:15 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Videos about My Work",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "https://lzrbit.github.io/videos/",
+        "teaser":"https://lzrbit.github.io/assets/images/default_teaser.jpg"},]
